7a3d2979265fa04d1808547398fabc37c091829a
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2025-11-13 20:47:21 +0100

    wip

--- src/modules/wallet/wallet.validation.ts
+++ src/modules/wallet/wallet.validation.ts
@@ -1,6 +1,7 @@
 import * as Joi from "joi";
 
 export const CreateWalletValidation = Joi.object().keys({
+  user_id: Joi.string().required(),
   network: Joi.string().required(),
   currency: Joi.string().required(),
   wallet_address: Joi.string().required(),
